1493d2cb06a90f472ad82094ee1245f459f4f893
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-05-18 03:42:43 +0700

    feat: working aoe move support

--- src/classes/Move.js
+++ src/classes/Move.js
@@ -18,28 +18,49 @@ export default function Move({ name, action }) {
     const size = Math.sqrt(Object.keys(tileMap).length);
     const userIndex = coordinateToIndex(user.coordinate, size);
     const clickedTileIndex = coordinateToIndex(targetTile.coordinate, size);
-    const entityUpdates = [];
 
-    tileMap[userIndex].entity = {};
-
-    if (Object.keys(targetTile.entity).length === 0) {
-      tileMap[clickedTileIndex].entity = user;
-      user.coordinate = targetTile.coordinate;
-    } else {
-      if (targetTile.entity.health > action.damage) {
-        const collisionResult = calculateCollisionResult(
-          user.coordinate,
-          targetTile.coordinate
-        );
-        const collisionResultIndex = coordinateToIndex(collisionResult, size);
-        tileMap[collisionResultIndex].entity = user;
-        user.coordinate = collisionResult;
-        targetTile.entity.health = targetTile.entity.health - action.damage;
-      } else {
-        user.coordinate = targetTile.coordinate;
-        targetTile.entity.health = 0;
-        tileMap[clickedTileIndex].entity = user;
-      }
+    switch (action.type) {
+      case "attack":
+        tileMap[userIndex].entity = {};
+        if (Object.keys(targetTile.entity).length === 0) {
+          tileMap[clickedTileIndex].entity = user;
+          user.coordinate = targetTile.coordinate;
+        } else {
+          if (targetTile.entity.health > action.damage) {
+            const collisionResult = calculateCollisionResult(
+              user.coordinate,
+              targetTile.coordinate
+            );
+            const collisionResultIndex = coordinateToIndex(
+              collisionResult,
+              size
+            );
+            tileMap[collisionResultIndex].entity = user;
+            user.coordinate = collisionResult;
+            targetTile.entity.health = targetTile.entity.health - action.damage;
+          } else {
+            user.coordinate = targetTile.coordinate;
+            targetTile.entity.health = 0;
+            tileMap[clickedTileIndex].entity = user;
+          }
+        }
+        break;
+      case "collateral":
+        const area = getClickableArea(user, size);
+        for (let tile of area) {
+          const idx = coordinateToIndex(tile, size);
+          const tileEntity = tileMap[idx].entity;
+          if (
+            Object.keys(tileEntity).length !== 0 ||
+            tileEntity.constructor !== Object
+          ) {
+            tileEntity.health -= action.damage;
+            if (tileEntity.health <= 0) {
+              tileMap[idx].entity = {};
+            }
+          }
+        }
+        break;
     }
   }
 
